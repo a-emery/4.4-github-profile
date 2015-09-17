8d10a662ad9f5b1d0d1679548a66a4d9360232f7
Aaron Emery <aemery14@gmail.com>
2015-09-17 15:43:56 -0400

    Finished styling header

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,14 @@
 $.ajax({
   method: "GET",
   url: "https://api.github.com/user/repos",
-  dataType: "json",
+  headers: { "Authorization": "token b4102a66069d10680913154dd6d6f42e7c14d363" }
+}).then(function(datShit){
+  console.log(datShit);
+});
+
+$.ajax({
+  method: "GET",
+  url: "https://api.github.com/user",
   headers: { "Authorization": "token b4102a66069d10680913154dd6d6f42e7c14d363" }
 }).then(function(datShit){
   console.log(datShit);
